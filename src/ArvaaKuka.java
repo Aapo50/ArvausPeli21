9a7c17eada309fc2bf53c38c82305b4430b6b6a9
Aapo50 <aapo.leppikallio@student.hamk.fi>
2023-09-06 14:47:41 +0300

    Eka commit

--- src/ArvaaKuka.java
+++ src/ArvaaKuka.java
@@ -1,9 +1,30 @@
-
+import java.util.Random;
+import java.util.Scanner;
 public class ArvaaKuka {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Random r = new Random();
+		Scanner in = new Scanner(System.in);
+		
+		String vastaus;
+		int guess = 0;
+		do
+		{
+			System.out.println("Guess my name (type stop to exit)");
+			vastaus = in.nextLine();
+		}
+		while(!vastaus.equals("stop"));
+			{
+			
+			}
+		
+		
+		
+		
+		
+		
 	}
 
 }
